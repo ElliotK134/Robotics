7c30b3e4efa6ffd330f59ccc6c29865dbafd58fe
ElliotK134 <13490022@students.lincoln.ac.uk>
2019-03-14 19:19:24 +0000

    Update action.py

--- action.py
+++ action.py
@@ -74,7 +74,7 @@ class Search:
             lower_yellow = np.array([20, 120, 100])
             upper_yellow = np.array([30, 360, 360])
             yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-            if 'red' in self.colours_to_find and 'blue' in self.colours_to_find:
+            if 'red' in self.colours_to_find or 'blue' in self.colours_to_find:
                 self.mask = self.mask + yellow_mask
             else:
                 self.mask = yellow_mask
@@ -83,7 +83,7 @@ class Search:
             lower_green = np.array([50, 120, 100])
             upper_green = np.array([65, 360, 360])
             green_mask = cv2.inRange(hsv, lower_green, upper_green)
-            if 'red' in self.colours_to_find and 'blue' in self.colours_to_find and 'yellow' in self.colours_to_find:
+            if 'red' in self.colours_to_find or 'blue' in self.colours_to_find or 'yellow' in self.colours_to_find:
                 self.mask = self.mask + green_mask
             else:
                 self.mask = green_mask
@@ -100,7 +100,7 @@ class Search:
 
     def spin(self):
         # publish an empty goal so nothing happens with action client in the mean time
-        self.client.send_goal_and_wait(move_base_msgs.msg.MoveBaseGoal())
+        # self.client.send_goal_and_wait(move_base_msgs.msg.MoveBaseGoal())
         rate = rospy.Rate(40)
         twist_msg = Twist()
         twist_msg.linear.x = 0.0
@@ -122,40 +122,40 @@ class Search:
             self.mask[0:search_top, 0:w] = 0
             self.mask[search_bot:h, 0:w] = 0
             M = cv2.moments(self.mask)
-            if M['m00'] > 0:  # If the area is greater than 0
+            if M['m00'] > 5000:  # If the area is greater than 0
+                twist_msg.angular.z = 0
+                self.twist_pub.publish(twist_msg)
                 self.move_to_pole()
+                break
             self.twist_pub.publish(twist_msg)
-            if self.orientation < orientation + 0.1 and self.orientation > orientation - 0.1:
+            if self.orientation < orientation + 0.05 and self.orientation > orientation - 0.05:
+                print(self.orientation)
                 break 
             
-
-
-
 
         
     def move_to_pole(self):
         print("IN FUNCTION")
-        # publish an empty goal so nothing happens with action client in the mean time
-        self.client.send_goal_and_wait(move_base_msgs.msg.MoveBaseGoal())
+        self.client.cancel_all_goals()
         h, w, d = self.cv_image.shape
         search_top = h / 4
         search_bot = 3*h/4 + 20
         self.mask[0:search_top, 0:w] = 0
         self.mask[search_bot:h, 0:w] = 0
         M = cv2.moments(self.mask)
-        if M['m00'] > 0:  # If the area is greater than 0
+        print(M['m00'])
+        if M['m00'] > 5000:  # If the area is greater than 0
             # find the x and y co-ordinates of the centroid of the region
             cx = int(M['m10']/M['m00']) 
             cy = int(M['m01']/M['m00'])
-            depth = self.depth[cy, cx]
-            while depth > 0.9:
+            test = False
+            while not test:
                 self.mask[0:search_top, 0:w] = 0
                 self.mask[search_bot:h, 0:w] = 0
                 M = cv2.moments(self.mask)
                 if M['m00'] > 0: 
                     cx = int(M['m10']/M['m00']) 
                     cy = int(M['m01']/M['m00'])
-                print(depth)
                 # Calculate the error, or how much the robot needs to turn to get the object at the center of its vision
                 error = cx - w/2
                 # Publish a twist command telling the robot to move to the pole
@@ -166,6 +166,7 @@ class Search:
                 # now use the depth image to see how far the robot is from the object at the centroid calculated earlier
                 # if the robot is under 1 meter away, stop
                 depth = self.depth[cy, cx]
+                print(depth)
                 if depth < 1.0:
                     twist_msg.linear.x = 0.0
                     twist_msg.angular.z = 0.0
@@ -175,19 +176,25 @@ class Search:
                         print("found yellow")
                         self.colours_to_find = [i for i in self.colours_to_find if i != 'yellow']
                         print(self.colours_to_find)
+                        test = True
                     if np.all(self.threshImg[cy, cx] == [0, 102, 0]):
                         print("found green")
                         self.colours_to_find = [i for i in self.colours_to_find if i != 'green']
                         print(self.colours_to_find)
+                        test = True
                     if np.all(self.threshImg[cy, cx] == [0, 0, 102]):
                         print("found red")
                         self.colours_to_find = [i for i in self.colours_to_find if i != 'red']
                         print(self.colours_to_find)
+                        test = True
 
                     self.twist_pub.publish(twist_msg)
-        self.client.cancel_goal()
+        print("exiting function")
+
     def odom_cb(self, data):
         self.orientation = data.pose.pose.orientation.z
+        self.posx = data.pose.pose.position.x
+        self.posy = data.pose.pose.position.y
 
 
            
@@ -199,10 +206,10 @@ if __name__ == "__main__":
 
     while search.colours_to_find:
         
+        print "publishing goal"
+        search.move_client(-4,5)
+        search.spin()
         search.move_client(-4,0)
         search.spin()
-        search.move_client(2,-5)
-        search.spin()
 
-    rospy.spin()
-    
+rospy.spin()
